314f59ebd581b155c32003baa84c90199dcc11a5
Joe Rumsey <gitub@rumsey.org>
2011-10-22 22:29:40 -0700

    'buzz' should be the thing require()'ed

--- testapp.lua
+++ testapp.lua
@@ -1,17 +1,17 @@
 #!/Users/ogre/src/luajit-2.0/src/luajit
-require('server')
+require('buzz')
 
 function index(request)
   print('index request')
-  server.response(request, '<html><h1>Hello, World!</h1></html>\n')
+  buzz.response(request, '<html><h1>Hello, World!</h1></html>\n')
 end
 
 function testarg(request, args)
   print('arg request (arg=\''..args[1]..'\')')
-  server.response(request, '<html><h1>arg='..args[1]..'</h1></html>\n')
+  buzz.response(request, '<html><h1>arg='..args[1]..'</h1></html>\n')
 end
 
-server.get('/', index)
-server.get('/testarg/(.*)$', testarg)
+buzz.get('/', index)
+buzz.get('/testarg/(.*)$', testarg)
 
-server.run()
+buzz.run()
