7dba36599fd0ed03e3e3f3f542de28258bd78c93
Joe Rumsey <gitub@rumsey.org>
2011-10-23 10:34:38 -0700

    Patterns need beginning of line marker

--- testapp.lua
+++ testapp.lua
@@ -6,17 +6,17 @@ function index(request)
   print('index request')
   buzz.response(request, '<html><h1>Hello, World!</h1></html>\n')
 end
-buzz.get('/$', index)
+buzz.get('^/$', index)
 
 -- Create a handler that takes one argument
 function testarg(request, args)
   print('arg request (arg=\''..args[1]..'\')')
   buzz.response(request, '<html><h1>arg='..args[1]..'</h1></html>\n')
 end
-buzz.get('/testarg/(.*)/?$', testarg)
+buzz.get('^/testarg/(.*)/?$', testarg)
 
 -- Create a handler for two arguments, define the handler inline
-buzz.get('/test2args/(.*)/(.*)/$', 
+buzz.get('^/test2args/(.*)/(.*)/$', 
 		 function(request, args)
 		   print('test2args (arg1=\''..args[1]..'\', arg2=\''..args[2]..'\')')
 		   buzz.response(request, '<html><h1>arg1=\''..args[1]..'\', arg2=\''..args[2]..'\'</h1></html>\n')
